ef948603bc541f21f674289143725583dc6cca68
saad <saadneosoft@gmail.com>
2023-10-19 10:49:59 +0530

    Initial Commit

---
+++ BasicFunctionalityInSwiftUi/BasicFunctionalityInSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicFunctionalityInSwiftUiApp.swift
+//  BasicFunctionalityInSwiftUi
+//
+//  Created by Neosoft on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BasicFunctionalityInSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

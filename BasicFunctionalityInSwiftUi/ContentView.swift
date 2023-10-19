ea1a0fad6145e52e0f1ced21e39c0985cc7f7a4a
saad <saadneosoft@gmail.com>
2023-10-19 13:25:42 +0530

    basic with @Static Var Explanation

--- BasicFunctionalityInSwiftUi/ContentView.swift
+++ BasicFunctionalityInSwiftUi/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var isStatus : Bool = false
+    @State private var playStatus : String = "Not Playing"
+    var body : some View{
+        VStack{
+            Image("music")
+                .resizable()
+                .frame(width: 250, height: 250)
+            Text(self.playStatus)
+                .font(.title)
+                .bold()
+            
+            HStack{
+                Button {
+                    print("Backwerd Clicked")
+                } label: {
+                    Image("backwardRewid")
+                        .background(Color.gray)
+                        .cornerRadius(10)
+                }.padding()
+
+                Button {
+                    if isStatus{
+                        playStatus = "Not Playing"
+                        isStatus = false
+                    }else{
+                        playStatus = "Playing"
+                        isStatus = true
+                    }
+                } label: {
+                    if isStatus{
+                        Image("pause")
+                    }else{
+                        Image("play")
+                    }
+                }
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                
+                Button {
+                    print("Forward Clicked")
+                } label: {
+                    Image("forwardRewind")
+                        .background(Color.gray)
+                        .cornerRadius(10)
+                }.padding()
+            }
+            
         }
-        .padding()
     }
 }
 

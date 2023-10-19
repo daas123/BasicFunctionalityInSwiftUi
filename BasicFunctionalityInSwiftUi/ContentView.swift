87e3007350d4e9aa0b689f348c18b545add56bb0
saad <saadneosoft@gmail.com>
2023-10-19 14:49:42 +0530

    Done For @obseervable @observer @published Done

--- BasicFunctionalityInSwiftUi/ContentView.swift
+++ BasicFunctionalityInSwiftUi/ContentView.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject private  var userName : User = User()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack{
+            Text("Your Full Name is : \(userName.name)")
+                .padding()
+            TextField("Enter Full Name", text: $userName.name)
+                .padding()
+        }.padding()
     }
 }
 
---
+++ BasicFunctionalityInSwiftUi/Model.swift
@@ -0,0 +1,11 @@
+//
+//  Model.swift
+//  BasicFunctionalityInSwiftUi
+//
+//  Created by Neosoft on 19/10/23.
+//
+
+import Foundation
+class User : ObservableObject{
+    @Published var name : String = String()
+}
